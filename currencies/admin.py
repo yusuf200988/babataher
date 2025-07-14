bd4ff8cd6d55238e1542740f40e3d358bbeea1dd
yousef <yousef.mohammady@hotmail.com>
2025-07-14 16:54:19 +0330

    add last price

--- currencies/admin.py
+++ currencies/admin.py
@@ -5,5 +5,5 @@ from .models import Currency
 
 @admin.register(Currency)
 class CurrencyAdmin(admin.ModelAdmin):
-    list_display = ['title', 'price', 'updated_time']
+    list_display = ['title', 'current_price', 'last_price', 'updated_time']
     search_fields = ['title']
--- currencies/models.py
+++ currencies/models.py
@@ -5,7 +5,8 @@ class Currency(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     avatar = models.ImageField(upload_to='media/')
-    price = models.IntegerField()
+    current_price = models.IntegerField()
+    last_price = models.IntegerField()
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
 
@@ -13,6 +14,11 @@ class Currency(models.Model):
         db_table = 'currencies'
         verbose_name = 'Currency'
         verbose_name_plural = 'Currencies'
+
+    def save(self, *args, **kwargs):
+        if self.last_price is None:
+            self.last_price = self.current_price
+            super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
